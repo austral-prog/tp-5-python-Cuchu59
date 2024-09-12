762d26c28c51b869962d7445124f120588210fec
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-09-12 20:11:24 -0300

    Update User.py

--- src/User.py
+++ src/User.py
@@ -6,21 +6,21 @@ class User:
         self.__number_of_checkins = number_of_checkins
 
     # Getters
-    def get_dni(self):
+    def get_dni(self) -> int:
         return self.__dni
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.__name
 
-    def get_number_of_checkouts(self):
+    def get_number_of_checkouts(self) -> int:
         return self.__number_of_checkouts
 
-    def get_number_of_checkins(self):
+    def get_number_of_checkins(self) -> int:
         return self.__number_of_checkins
 
     # Setters
-    def increment_checkouts(self):
+    def increment_checkouts(self) -> None:
         pass
 
-    def increment_checkins(self):
+    def increment_checkins(self) -> None:
         pass
