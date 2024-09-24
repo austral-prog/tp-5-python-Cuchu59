e9c2462069a9ab68cc97aea31478122355ce4f8d
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-09-24 17:45:26 -0300

    Update Book.py

--- src/Book.py
+++ src/Book.py
@@ -1,40 +1,45 @@
-# from typing import Self
-
-
 class Book:
-    def __init__(self, isbn :int, title :str, author :str, available :bool =True , checkout_num :int=0):
-        self.__isbn = isbn
-        self.__title = title
-        self.__author = author
-        self.__available = available
-        self.__checkout_num = checkout_num
+    def init(self, isbn: str, title: str, author: str, available: bool = True, checkout_num: int = 0) -> None:
+        self.isbn: str = isbn
+        self.title: str = title
+        self.author: str = author
+        self.available: bool = available
+        self.checkout_num: int = checkout_num
 
     # Getters
-    def get_isbn(self) -> int:
-        return self.__isbn
+    def get_isbn(self) -> str:
+        return self.isbn
 
     def get_title(self) -> str:
-        return self.__title
+        return self.title
 
     def get_author(self) -> str:
-        return self.__author
+        return self.author
 
     def is_available(self) -> bool:
-        return self.__available
+        return self.available
 
     def get_checkout_num(self) -> int:
-        return self.__checkout_num
+        return self.checkout_num
 
     # Setters
     def set_available(self, available: bool) -> None:
-        self.__available = available
+        self.available = available
 
     def increment_checkout_num(self) -> None:
-        self.__checkout_num += 1
+        self.checkout_num += 1
 
     # Utils
-    def __str__(self) -> str:
-        return f"ISBN: {self.__isbn}, Title: {self.__title}, Author: {self.__author}"
+    def str(self) -> str:
+        return (f"ISBN: {self.isbn}, Title: {self.title}, "
+                f"Author: {self.author}, Available: {self.available}, "
+                f"Checkout Number: {self.checkout_num}")
 
-    def __eq__(self, other: "Book") -> bool:
-        return self.__isbn
+    def eq(self, other: object) -> bool:
+        if isinstance(other, Book):
+            return (self.isbn == other.isbn and
+                    self.title == other.title and
+                    self.author == other.author and
+                    self.available == other.available and
+                    self.checkout_num == other.__checkout_num)
+        return False
