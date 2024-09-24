e19bfbfb9aac49f8cb3578437318969114a9ec25
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-09-24 17:44:39 -0300

    Update User.py

--- src/User.py
+++ src/User.py
@@ -1,26 +1,26 @@
 class User:
-    def __init__(self, dni, name, number_of_checkouts=0, number_of_checkins=0):
-        self.__dni = dni
-        self.__name = name
-        self.__number_of_checkouts = number_of_checkouts
-        self.__number_of_checkins = number_of_checkins
+    def init(self, dni: int, name: str, number_of_checkouts: int = 0, number_of_checkins: int = 0) -> None:
+        self.dni: int = dni
+        self.name: str = name
+        self.number_of_checkouts: int = number_of_checkouts
+        self.number_of_checkins: int = number_of_checkins
 
     # Getters
     def get_dni(self) -> int:
-        return self.__dni
+        return self.dni
 
     def get_name(self) -> str:
-        return self.__name
+        return self.name
 
     def get_number_of_checkouts(self) -> int:
-        return self.__number_of_checkouts
+        return self.number_of_checkouts
 
     def get_number_of_checkins(self) -> int:
-        return self.__number_of_checkins
+        return self.number_of_checkins
 
     # Setters
     def increment_checkouts(self) -> None:
-        pass
+        self.number_of_checkouts += 1
 
     def increment_checkins(self) -> None:
-        pass
+        self.number_of_checkins += 1
